1671bc95ce936449bb8c1645e70bfc7065031feb
松下望 <matsu.uner@gmail.com>
2018-03-25 05:36:23 +0900

    環境構築

---
+++ development.js
@@ -0,0 +1,35 @@
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin' //この行を追記
+
+const src  = path.resolve(__dirname, 'src')
+const dist = path.resolve(__dirname, 'dist')
+
+export default {
+  entry: src + '/index.jsx',
+
+  output: {
+    path: dist,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js']
+  },
+
+  plugins: [
+      new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
+}
