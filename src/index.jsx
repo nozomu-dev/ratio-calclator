900e06e60e401e07628284431677adf81b559bf7
松下望 <matsu.uner@gmail.com>
2018-03-26 09:51:43 +0900

    テキストコンポーネントを作成

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+let ratio = 1.618;
+const x1 = ratio;
+const x2 = ratio * ratio;
+const x3 = x2 * ratio;
+const x4 = x3 * ratio;
+const x5 = x4 * ratio;
+
+
+
+/**
+ * Class Components
+ */
+
 class InputFontsize extends React.Component {
     constructor(props) {
         super(props);
@@ -43,11 +56,65 @@ class InputLineHeight extends React.Component {
     }
 }
 
-class SideBar extends React.Component {
+
+
+/**
+ * Functional Components
+ */
+
+ const HorizontallGridLayer = (props) => {
+     return (
+         <div></div>
+     );
+ }
+
+const VerticalGridLayer = (props) => {
+    return (
+        <div></div>
+    );
+}
+
+
+const StyleGenerator = ({size}) => ({
+    fontSize: size
+});
+
+const TitleFirst = (props) => {
+    return <h1 style={StyleGenerator(props)}>Title text <span className="title-label">{props.size}px</span></h1>;
+}
+
+const TitleSecond = (props) => {
+    return <h2 style={StyleGenerator(props)}>Title text<span className="title-label">{props.size}px</span></h2>
+}
+
+const TitleThird = (props) => {
+    return <h3 style={StyleGenerator(props)}>Title text<span className="title-label">{props.size}px</span></h3>;
+}
+
+const TextMain = (props) => {
+    return (
+        <p style={StyleGenerator(props)}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est} laborum.
+        </p>
+    );
+}
+
+const TextSmall = (props) => {
+    return <small style={StyleGenerator(props)}>This is small text.</small>;
+}
+
+/**
+ * Parent Components
+ */
+
+class Container extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            use: 'ratio-gold',
             fontsize: 16,
             lineHeight: 'ratio',
             containerWidth: 980,
@@ -55,6 +122,7 @@ class SideBar extends React.Component {
             gutter: 'line-height'
         };
         this.update = this.update.bind(this);
+        console.log(this.state.styles)
     }
 
     update(key, value) {
@@ -67,9 +135,15 @@ class SideBar extends React.Component {
             <div>
                 <InputFontsize default={this.state.fontsize} update={this.update} />
                 <InputLineHeight default={this.state.lineHeight} update={this.update} />
+                <TitleFirst size={this.state.fontsize * x4} />
+                <TitleSecond size={this.state.fontsize * x3} />
+                <TitleThird size={this.state.fontsize * x2} />
+                <TextMain size={this.state.fontsize} />
+                <TextSmall size={this.state.fontsize / x1} />
             </div>
         );
     }
 }
 
-render(<SideBar />, document.getElementById('app'));
+
+render(<Container />, document.getElementById('app'));
